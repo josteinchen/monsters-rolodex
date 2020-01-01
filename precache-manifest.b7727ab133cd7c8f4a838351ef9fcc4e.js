aaa61476970a005701c4d85adcce84fbef2cc058
Jostein Chen <jostein.chen@gmail.com>
2020-01-01 17:06:08 +0800

    Updates

---
+++ precache-manifest.b7727ab133cd7c8f4a838351ef9fcc4e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd3206c088aacc324973588a3bbe024e",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f3bf605d931b861bdb97",
+    "url": "/monsters-rolodex/static/css/main.85a25ad5.chunk.css"
+  },
+  {
+    "revision": "d499d87a993599493776",
+    "url": "/monsters-rolodex/static/js/2.1e4a2dbe.chunk.js"
+  },
+  {
+    "revision": "f3bf605d931b861bdb97",
+    "url": "/monsters-rolodex/static/js/main.0df54937.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
